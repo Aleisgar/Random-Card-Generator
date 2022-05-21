776d95e33da40559008349668a0e14a02a26099a
Aleisgar <aleisgar@gmail.com>
2022-05-21 17:03:09 +0000

    create repository

--- src/app.js
+++ src/app.js
@@ -6,6 +6,38 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let posicion = Math.floor(Math.random() * numbers.length);
+  let cardNumber = document.querySelector(".number");
+  cardNumber.innerHTML = numbers[posicion];
+
+  let icons = ["♦", "♥", "♠", "♣"];
+  let indexicon = Math.floor(Math.random() * icons.length);
+  let cardicontop = document.querySelector(".top-icon");
+  let cardiconbottom = document.querySelector(".bottom-icon");
+  cardicontop.innerHTML = icons[indexicon];
+  cardiconbottom.innerHTML = icons[indexicon];
+
+  icons[indexicon] === "♦" || icons[indexicon] === "♥"
+    ? (cardicontop.style.color = "red")
+    : (cardicontop.style.color = "black");
+
+  icons[indexicon] === "♦" || icons[indexicon] === "♥"
+    ? (cardiconbottom.style.color = "red")
+    : (cardiconbottom.style.color = "black");
 };
